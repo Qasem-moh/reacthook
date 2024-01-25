7b731fc82172ea0d908356b833d8d8b2871b2f91
Qasem Mohammd <75634309+Qasem-moh@users.noreply.github.com>
2024-01-25 16:38:36 +0300

    hook

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Hook from "./Components/Hook";
 import MouseMoveHook from "./Components/mouseMoveHook";
+import IntervalHookCounter from "./Components/IntervalHookCounter";
 
 function App() {
     return (<div className="App">
             <h2>Hello</h2>
         {/*<Hook/>*/}
-        <MouseMoveHook/>
+        {/*<MouseMoveHook/>*/}
+        <IntervalHookCounter/>
         </div>);
 }
 
