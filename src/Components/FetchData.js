2ff63ac2e5fb95c6f52a29b4cab13400ca1c20e9
Qasem Mohammd <75634309+Qasem-moh@users.noreply.github.com>
2024-01-27 15:38:43 +0300

    hook

--- src/Components/FetchData.js
+++ src/Components/FetchData.js
@@ -4,21 +4,27 @@ import axios from "axios";
 function FetchData(props) {
     const [data, setData] = useState({})
     const [id, setId] = useState(1)
+    const [isButtonClicked, setIsButtonClicked] = useState()
+
+    const handleFetch=()=>{
+        setIsButtonClicked(id)
+    }
 
     useEffect(() => {
         // axios.get('https://jsonplaceholder.typicode.com/posts')
-        axios.get(`https://jsonplaceholder.typicode.com/posts/${id}`)
+        axios.get(`https://jsonplaceholder.typicode.com/posts/${isButtonClicked}`)
             .then((response) => {
                 setData(response.data)
                 console.log(response.data)
             }).catch(err => {
             console.log(err)
         })
-    }, [id])
+    }, [isButtonClicked])
     console.log(data)
     return (
         <div>
             <input name="id" value={id} onChange={e=>setId(e.target.value)}/>
+            <button onClick={handleFetch} >Fetch Data</button>
 <h3>{data.body}</h3>
             {/*{*/}
             {/*    data.map((item) => {*/}
