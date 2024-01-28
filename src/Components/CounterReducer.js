da27f23de2e4039447937554d5664822ab408eff
Qasem Mohammd <75634309+Qasem-moh@users.noreply.github.com>
2024-01-28 16:18:43 +0300

    hook

--- src/Components/CounterReducer.js
+++ src/Components/CounterReducer.js
@@ -1,13 +1,14 @@
 import React, {useReducer} from 'react';
+
 const initialState = {
     firstCounter: 0
 }
 const counterReducer = (state, action) => {
     switch (action.type) {
         case 'INCREMENT':
-            return {firstCounter: state.firstCounter + 1}
+            return {firstCounter: state.firstCounter + action.value}
         case 'DECREMENT':
-            return {firstCounter: state.firstCounter - 1}
+            return {firstCounter: state.firstCounter - action.value}
         case 'RESET':
             return initialState
         default:
@@ -23,8 +24,10 @@ function CounterReducer(props) {
             <div>
                 <h2>{count.firstCounter}</h2>
 
-                <button onClick={() => dispatch({type: 'INCREMENT'})}>Increment</button>
-                <button onClick={() => dispatch({type: 'DECREMENT'})}>Decrement</button>
+                <button onClick={() => dispatch({type: 'INCREMENT', value: 1})}>Increment</button>
+                <button onClick={() => dispatch({type: 'INCREMENT', value: 5})}>Increment +5</button>
+                <button onClick={() => dispatch({type: 'DECREMENT', value: 1})}>Decrement</button>
+                <button onClick={() => dispatch({type: 'DECREMENT', value: 5})}>Decrement -5</button>
                 <button onClick={() => dispatch({type: 'RESET'})}>Reset</button>
 
 
